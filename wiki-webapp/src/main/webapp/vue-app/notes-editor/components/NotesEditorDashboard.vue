d57557135fa41f23f47c9c151493a6abd289b23b
saraBoutej <sboutej@exoplatform.com>
2021-06-14 15:19:12 +0100

    Notes: Fix notes editor padding

--- wiki-webapp/src/main/webapp/vue-app/notes-editor/components/NotesEditorDashboard.vue
+++ wiki-webapp/src/main/webapp/vue-app/notes-editor/components/NotesEditorDashboard.vue
@@ -3,8 +3,8 @@
     <div>
       <div
         id="notesEditor"
-        class="notesEditor white width-full">
-        <div class="notesActions">
+        class="notesEditor width-full">
+        <div class="notesActions white">
           <div class="notesFormButtons d-inline-flex flex-wrap width-full pa-3 ma-0">
             <div class="notesFormLeftActions d-inline-flex mr-10">
               <img :src="srcImageNote">
@@ -32,8 +32,8 @@
             </div>
           </div>
         </div>
-        <div id="notesTop"></div>
-        <div class="formInputGroup overflow-auto ma-2 pa-2 flex">
+        <div id="notesTop" class="width-full"></div>
+        <div class="formInputGroup white overflow-auto ma-2 pa-2 flex">
           <textarea
             id="notesContent"
             v-model="notes.content"
