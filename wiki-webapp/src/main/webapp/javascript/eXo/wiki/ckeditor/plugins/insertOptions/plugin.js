6698fa0eabb8ec1211546d4e7986fd1859f0766e
lbengayed <lbengayed@exoplatform.com>
2021-07-06 09:47:25 +0100

    Task-46466-45965:Add + button for Insert options and Display Only functional features in Insert Options drawer

--- wiki-webapp/src/main/webapp/javascript/eXo/wiki/ckeditor/plugins/insertOptions/plugin.js
+++ wiki-webapp/src/main/webapp/javascript/eXo/wiki/ckeditor/plugins/insertOptions/plugin.js
@@ -1,13 +1,3 @@
-/**
- * Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
- * Licensed under the terms of the MIT License (see LICENSE.md).
- *
- *
- * Created out of the CKEditor Plugin SDK:
- * https://ckeditor.com/docs/ckeditor4/latest/guide/plugin_sdk_intro.html
- */
-
-// Register the plugin within the editor.
 CKEDITOR.plugins.add( 'insertOptions', {
 
   // Register the icons. They must match command names.
@@ -17,7 +7,6 @@ CKEDITOR.plugins.add( 'insertOptions', {
   // The plugin initialization logic goes inside this method.
   init: function( editor ) {
 
-    // Define the editor command that inserts a timestamp.
     editor.addCommand( 'insertOptions', {
 
       // Define the function that will be fired when the command is executed.
