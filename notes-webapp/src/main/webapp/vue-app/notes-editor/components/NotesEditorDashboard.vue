94f9462a83e4f3fe175e747efb16c7eacdf379aa
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-01-17 18:04:22 +0100

    Task-52413: [BUG] Linked to edit note page with inserted automatic children (#457)

--- notes-webapp/src/main/webapp/vue-app/notes-editor/components/NotesEditorDashboard.vue
+++ notes-webapp/src/main/webapp/vue-app/notes-editor/components/NotesEditorDashboard.vue
@@ -23,7 +23,7 @@
               <span class="notesFormTitle ps-2">{{ noteFormTitle }}</span>
             </div>
             <div class="notesFormRightActions pr-7">
-              <p class="draftSavingStatus mr-7">{{ draftSavingStatus }}</p>
+              <p v-if="!initChildNavigation" class="draftSavingStatus mr-7">{{ draftSavingStatus }}</p>
               <button
                 id="notesUpdateAndPost"
                 class="btn btn-primary primary px-2 py-0"
@@ -149,7 +149,7 @@ export default {
       saveDraft: '',
       postKey: 1,
       navigationLabel: `${this.$t('notes.label.Navigation')}`,
-      noteNavigationDisplayed: false
+      initMacroChildCompleted: false
     };
   },
   computed: {
@@ -173,6 +173,9 @@ export default {
     alertMessageClass(){
       return  this.message.replace(/<[^>]*>/g, '').replace(/&nbsp;/g, '').trim().length > 45 ? 'lengthyAlertMessage' : '';
     },
+    initChildNavigation() {
+      return this.initMacroChildCompleted;
+    }
   },
   watch: {
     'note.title'() {
@@ -184,7 +187,7 @@ export default {
       if (this.note.content !== this.actualNote.content) {
         this.autoSave();
       }
-    },
+    }
   },
   created() {
     window.addEventListener('beforeunload', () => {
@@ -297,6 +300,10 @@ export default {
         return;
       }
 
+      if (this.initMacroChildCompleted) {
+        return;
+      }
+
       // close draft dropping related alert
       if (this.alertType === 'warning' && this.note.draftPage && this.alert) {
         this.alert = false;
@@ -305,11 +312,12 @@ export default {
       clearTimeout(this.saveDraft);
       this.saveDraft = setTimeout(() => {
         this.savingDraft = true;
+        this.initMacroChildCompleted = false;
         this.draftSavingStatus = this.$t('notes.draft.savingDraftStatus');
         this.$nextTick(() => {
           this.saveNoteDraft();
         });
-      }, this.autoSaveDelay);
+      }, 0);
     },
     getNote(id) {
       return this.$notesService.getLatestDraftOfPage(id).then(latestDraft => {
@@ -364,6 +372,7 @@ export default {
         if ((this.note.content.trim().length === 0)) {
           this.$notesService.getNoteById(this.noteId, '','','',true).then(data => {
             if (data && data.children && data.children.length) {
+              this.initMacroChildCompleted = true;
               CKEDITOR.instances['notesContent'].setData(childContainer);
               this.setFocus();
             }
@@ -496,9 +505,7 @@ export default {
           localStorage.setItem(`draftNoteId-${this.note.id}`, JSON.stringify(savedDraftNote));
         }).then(() => {
           this.savingDraft = false;
-          setTimeout(() => {
-            this.draftSavingStatus = this.$t('notes.draft.savedDraftStatus');
-          }, this.autoSaveDelay/2);
+          this.draftSavingStatus = this.$t('notes.draft.savedDraftStatus');
         }).catch(e => {
           console.error('Error when creating draft note: ', e);
           this.$root.$emit('show-alert', {
