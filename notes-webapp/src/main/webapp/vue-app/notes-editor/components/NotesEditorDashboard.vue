5f78e612a9572c8e05f14e96f50aa72582aae9b1
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-05-23 13:17:27 +0100

    Task-56988: Photo disappears when it is moved or resized in editor (#541)
    Fix feedbacks

--- notes-webapp/src/main/webapp/vue-app/notes-editor/components/NotesEditorDashboard.vue
+++ notes-webapp/src/main/webapp/vue-app/notes-editor/components/NotesEditorDashboard.vue
@@ -59,7 +59,7 @@
             </div>
           </div>
         </div>
-        <div id="notesTop" class="width-full"></div>
+        <div id="notesTop" class="width-full darkComposerEffect"></div>
       </div>
 
       <form class="notes-content">
@@ -278,18 +278,15 @@ export default {
 
   },
   mounted() {
-    this.init();
+    if (this.spaceId) {
+      this.init();
+    }
   },
   methods: {
     init() {
-      this.$spaceService.getSpaceById(this.spaceId).then(space => {
-        this.spaceGroupId = space.groupId;
-        this.initCKEditor();
-        const elementNewTop = document.getElementById('notesTop');
-        elementNewTop.classList.add('darkComposerEffect');
-        this.setToolBarEffect();
-        this.initDone = true;
-      });
+      this.initCKEditor();
+      this.setToolBarEffect();
+      this.initDone = true;
     },
     autoSave() {
       // No draft saving if init not done or in edit mode for the moment
@@ -317,6 +314,10 @@ export default {
     },
     getNote(id) {
       return this.$notesService.getLatestDraftOfPage(id).then(latestDraft => {
+        if (latestDraft.wikiType === 'group') {
+          this.spaceGroupId = latestDraft.wikiOwner;
+        }
+        this.init();
         // check if page has a draft
         latestDraft = Object.keys(latestDraft).length !== 0 ? latestDraft : null;
         if (latestDraft) {
@@ -337,6 +338,10 @@ export default {
     },
     getDraftNote(id) {
       return this.$notesService.getDraftNoteById(id).then(data => {
+        if (data.wikiType === 'group') {
+          this.spaceGroupId = data.wikiOwner;
+        }
+        this.init();
         this.fillNote(data);
       }).finally(() => {
         const messageObject = {
