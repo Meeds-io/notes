9c927e9ae60828b4aa86b55695e37a46a819feec
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-10-12 12:29:11 +0100

    feat: Enhance Editor loading behavior - MEED-2706 - Meeds-io/MIPs#93 (#778)
    This change will enhance loading effect behavior to avoid flickering
    effect when CKEDitor resources are loading.

--- notes-webapp/src/main/webapp/vue-app/note-page-view/components/NotePageViewApp.vue
+++ notes-webapp/src/main/webapp/vue-app/note-page-view/components/NotePageViewApp.vue
@@ -22,7 +22,7 @@
   <v-app v-if="canView">
     <v-hover v-slot="{ hover }">
       <v-card
-        :class="!edit && 'pa-4'"
+        :class="viewMode && 'pa-4'"
         min-width="100%"
         max-width="100%"
         min-height="72"
@@ -31,14 +31,18 @@
         flat>
         <note-page-edit
           v-if="edit"
+          ref="editor"
+          :class="editorBackgroundLoading && 'position-absolute l-0 r-0'"
+          :style="editorBackgroundLoading && 'z-index: -1;'"
           class="full-width"
-          @saved="edit = false"
-          @cancel="edit = false" />
-        <template v-else>
+          @saved="closeEditor"
+          @cancel="closeEditor" />
+        <template v-if="!editorReady || !edit">
           <note-page-header
             v-if="$root.initialized && canEdit"
-            :hover="hover"
-            @edit="edit = true" />
+            :hover="hover || editorLoading"
+            :loading="editorLoading"
+            @edit="openEditor" />
           <note-page-view
             v-if="hasNote"
             class="full-width overflow-hidden" />
@@ -51,6 +55,7 @@
 export default {
   data: () => ({
     edit: false,
+    editorReady: false,
   }),
   computed: {
     hasNote() {
@@ -62,12 +67,45 @@ export default {
     canView() {
       return this.$root.canEdit || (this.$root.initialized && this.hasNote);
     },
+    viewMode() {
+      return !this.edit || this.editorBackgroundLoading;
+    },
+    editorBackgroundLoading() {
+      return this.editorLoading && this.hasNote;
+    },
+    editorLoading() {
+      return this.edit && !this.editorReady;
+    },
   },
   watch: {
     edit() {
       if (this.edit) {
         this.$root.$emit('close-alert-message');
       }
+    },
+  },
+  created() {
+    this.$root.$on('notes-editor-ready', this.setEditorReady);
+    this.$root.$on('notes-editor-unloaded', this.setEditorNotReady);
+  },
+  methods: {
+    openEditor() {
+      this.editorReady = false;
+      this.$nextTick().then(() => this.edit = true);
+    },
+    closeEditor() {
+      this.editorReady = false;
+      this.$nextTick().then(() => this.edit = false);
+    },
+    setEditorReady() {
+      window.setTimeout(() => {
+        this.editorReady = true;
+      }, 50);
+    },
+    setEditorNotReady() {
+      window.setTimeout(() => {
+        this.editorReady = false;
+      }, 50);
     },
   },
 };
