eb822f4697a5291a324e08791e98e8cd3c95175e
Mohamed Amine Krout <mkrout@exoplatform.com>
2023-10-23 15:19:17 +0100

    feat: Draft of a new  translation never saved - EXO-66050 - Meeds-io/MIPs#70
    (cherry picked from commit 3085593b7b062e8148022268768fa7a0e96df83a)

--- notes-webapp/src/main/webapp/vue-app/notes-editor/components/NotesEditorDashboard.vue
+++ notes-webapp/src/main/webapp/vue-app/notes-editor/components/NotesEditorDashboard.vue
@@ -406,11 +406,7 @@ export default {
           this.fillNote(latestDraft);
           setTimeout(() => {
             this.displayDraftMessage();
-<<<<<<< HEAD
-          }, this.autoSaveDelay/2);         
-=======
-          }, this.autoSaveDelay/2);
->>>>>>> 4a51221d7 (feat: Draft with translation - EXO-66042 - Meeds-io/MIPs#70 (#808))
+          }, this.autoSaveDelay/2);    
           this.initActualNoteDone = true;
         } else {
           this.$notesService.getNoteById(id,this.slectedLanguage).then(data => {
@@ -890,9 +886,7 @@ export default {
         const targetPageId = this.note.targetPageId;
         this.removeLocalStorageCurrentDraft();
         this.$notesService.deleteDraftNote(this.note).then(() => {
-          this.getNoteLanguages();
           this.draftSavingStatus = '';
-<<<<<<< HEAD
           this.getNoteLanguages().then(() => {
             let lang = this.translations.find(item => item.value ===this.slectedLanguage);
             if (!lang){
@@ -912,21 +906,6 @@ export default {
             this.closeAlertMessage(); 
           });
       
-=======
-          //re-initialize data
-          if (targetPageId) {
-            this.closeAlertMessage();
-            this.getNote(targetPageId);
-          } else {
-            const parentNote = {
-              id: this.note.parentPageId,
-              wikiId: this.note.wikiId,
-              wikiOwner: this.note.wikiOwner,
-              wikiType: this.note.wikiType,
-            };
-            window.location.href = this.$notesService.getPathByNoteOwner(parentNote, this.appName).replace(/ /g, '_');
-          }
->>>>>>> 4a51221d7 (feat: Draft with translation - EXO-66042 - Meeds-io/MIPs#70 (#808))
         }).catch(e => {
           console.error('Error when deleting draft note', e);
         });
