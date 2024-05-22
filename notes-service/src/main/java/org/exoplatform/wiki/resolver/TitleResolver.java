ef16280bd6427d1275bf10b1d23b52d2f2cf9883
Ayoub Zayati <azayati@exoplatform.com>
2024-05-22 18:08:13 +0200

    feat: Allow to create notes with the same title - EXO-71935 - Meeds-io/MIPs#130

--- notes-service/src/main/java/org/exoplatform/wiki/resolver/TitleResolver.java
+++ notes-service/src/main/java/org/exoplatform/wiki/resolver/TitleResolver.java
@@ -1,23 +1,28 @@
-/*
- * Copyright (C) 2003-2010 eXo Platform SAS.
+ /**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package org.exoplatform.wiki.resolver;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TitleResolver {
 
@@ -32,7 +37,8 @@ public class TitleResolver {
     if (isEncoded) {
       title = URLDecoder.decode(title, StandardCharsets.UTF_8);
     }
-    return title.replaceAll("[^a-zA-Z0-9_\\-]", "_");
+    String titleSuffix = "_" + new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date(System.currentTimeMillis()));
+    return title.replaceAll("[^a-zA-Z0-9_\\-]", "_") + titleSuffix;
   }
 
 }
