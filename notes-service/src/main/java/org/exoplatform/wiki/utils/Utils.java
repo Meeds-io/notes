5e21f22d70adeb8e8cec49c9c1b805cd1718c2f4
azayati <azayati@exoplatform.com>
2024-08-31 00:37:17 +0100

    feat: Add NOTE_ID param when sending mention in note notification - EXO-73814 - Meeds-io/MIPs#128

--- notes-service/src/main/java/org/exoplatform/wiki/utils/Utils.java
+++ notes-service/src/main/java/org/exoplatform/wiki/utils/Utils.java
@@ -823,6 +823,8 @@ public class Utils {
                             note.getAuthor())
                     .append(MentionInNoteNotificationPlugin.SPACE_ID,
                             space.getId())
+                    .append(MentionInNoteNotificationPlugin.NOTE_ID,
+                            note.getId())
                     .append(MentionInNoteNotificationPlugin.NOTE_TITLE,
                             note.getTitle())
                     .append(MentionInNoteNotificationPlugin.AUTHOR_AVATAR_URL,
