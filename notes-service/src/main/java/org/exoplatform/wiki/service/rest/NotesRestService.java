11fde5bae21f2ca3780379b5fc3b8a956d072669
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-09-16 12:08:29 +0100

    fix: Properties of newly created note are not updated - EXO-74190 - Meed-io/MIPs#128 (#1107)
    Properties of newly created note are not updated

--- notes-service/src/main/java/org/exoplatform/wiki/service/rest/NotesRestService.java
+++ notes-service/src/main/java/org/exoplatform/wiki/service/rest/NotesRestService.java
@@ -30,6 +30,7 @@ import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.Set;
