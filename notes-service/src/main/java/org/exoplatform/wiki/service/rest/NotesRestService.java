b3d8256be1bd6fc0506e49c3f97ca015b3e74193
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-09-19 17:12:38 +0100

    fix: Treeview menu loading perf issue- EXO-73169 - Meeds-io/MIPs#129 (#1115)
    Treeview menu loading perf issue
    
    fix: Treeview menu loading perf issue- EXO-73169 - Meeds-io/MIPs#129

--- notes-service/src/main/java/org/exoplatform/wiki/service/rest/NotesRestService.java
+++ notes-service/src/main/java/org/exoplatform/wiki/service/rest/NotesRestService.java
@@ -29,6 +29,7 @@ import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -1226,12 +1227,13 @@ public class NotesRestService implements ResourceContainer {
 
       List<JsonNodeData> finalTree = new ArrayList<>();
       responseData = getJsonTree(noteParam, context);
-      JsonNodeData rootNodeData = responseData.get(0);
+      JsonNodeData rootNodeData = responseData.getFirst();
       rootNodeData.setHasDraftDescendant(true);
       finalTree.add(rootNodeData);
       context.put(TreeNode.DEPTH, "1");
 
-      List<JsonNodeData> children = new ArrayList<>(rootNodeData.getChildren());
+      List<JsonNodeData> listChildren = rootNodeData.getChildren();
+      List<JsonNodeData> children = listChildren != null ? new ArrayList<>(listChildren) : new ArrayList<>();
       List<JsonNodeData> parents = new ArrayList<>();
 
       do {
@@ -1299,7 +1301,7 @@ public class NotesRestService implements ResourceContainer {
                                  || Boolean.TRUE.equals(jsonNodeData.isHasDraftDescendant()))
                              .collect(Collectors.toList());
       }
-      while (bottomChildren.size() > 1 || (bottomChildren.size() == 1 && bottomChildren.get(0).getParentPageId() != null)) {
+      while (bottomChildren.size() > 1 || (bottomChildren.size() == 1 && bottomChildren.getFirst().getParentPageId() != null)) {
         for (JsonNodeData bottomChild : bottomChildren) {
           String parentPageId = bottomChild.getParentPageId();
           Optional<JsonNodeData> parentOptional = finalTree.stream()
