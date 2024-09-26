d0c12085dd7ca1b85f90e4f27f2e9d4dc141a0f6
jihed525 <jchabbeh@exoplatform.com>
2024-09-26 17:10:05 +0100

    fix: Treeview menu loading perf issue - EXO-73169 - Meeds-io/meeds#2411

--- notes-service/src/main/java/org/exoplatform/wiki/jpa/dao/DraftPageDAO.java
+++ notes-service/src/main/java/org/exoplatform/wiki/jpa/dao/DraftPageDAO.java
@@ -116,4 +116,9 @@ public class DraftPageDAO extends WikiBaseDAO<DraftPageEntity, Long> {
     }
   }
 
+  public Long countDraftPagesByParentPage(long parentPageId) {
+    return (Long) getEntityManager().createNamedQuery("wikiDraftPage.countDraftPagesByParentPage")
+                                    .setParameter("parentPageId", parentPageId)
+                                    .getSingleResult();
+  }
 }
