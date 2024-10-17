323c36af2cbcdd16e035596e856fc0d461991fb0
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2024-10-17 10:23:05 +0200

    fix: Fix empty note without children displays the TOC of home- EXO-74728 - Meeds-io/MIPs#161 (#1176)

--- notes-webapp/src/main/webapp/vue-app/notes/components/NotesOverview.vue
+++ notes-webapp/src/main/webapp/vue-app/notes/components/NotesOverview.vue
@@ -1002,6 +1002,9 @@ export default {
         if (data?.jsonList?.length) {
           this.noteChildren = data?.jsonList;
         }
+        else {
+          this.noteChildren = [];
+        }
       });
     },
     openNoteChild(item) {
