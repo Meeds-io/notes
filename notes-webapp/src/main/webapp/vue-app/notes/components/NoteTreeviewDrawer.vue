6a1afb287541059b7a14bf9e7c257c99d96fefee
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-11-18 10:37:19 +0100

    feat: Implement publish note - EXO-73041 - Meeds-io/MIPs#161 (#1212)
    Implement publish note
    
    feat: Add feature flag for publish note option - EXO-73041 - Meeds-io/MIPs#161 (#1219)
    
    Add feature flag for publish note optio
    
    fix: Fix enable condition of delete and import/export action menu options - EXO-73041 - Meeds-io/MIPs#161
    
    fix enable condition of delete/import/export action menu options
    
    feat: Refactor publish note option integration - EXO-73041 - Meeds-io/Meeds#161 (#1220)
    
    Refactor publish note option integration

--- notes-webapp/src/main/webapp/vue-app/notes/components/NoteTreeviewDrawer.vue
+++ notes-webapp/src/main/webapp/vue-app/notes/components/NoteTreeviewDrawer.vue
@@ -701,6 +701,7 @@ export default {
           this.$root.$emit('open-note-by-name', noteName, note.draftPage);
           this.$refs.breadcrumbDrawer.close();
         }
+        document.dispatchEvent(new CustomEvent('note-navigation-updated', {detail: note}));
       } else {
         this.$refs.breadcrumbDrawer.close();
       }
