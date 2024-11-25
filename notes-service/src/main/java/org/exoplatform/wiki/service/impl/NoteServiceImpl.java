dc8083de45ef076365a64efa840a2e97c082b115
Helmi Akermi <hakermi@exoplatform.com>
2024-11-25 12:26:25 +0100

    fix: Fix featured image is deleted in case of creating new page - EXO-75508 - Meeds-io/meeds#2614
    Prior to this change, when create new page from draft with featured image and try to delete the draft after saving the new page, the featured image is deleted with the draft,
    which shoudl not be the case when the featured image is linked to the original saved page, this due to wrong condition handling on the removeFeaturedImage function in case of
    draft of new page.
    This PR treats the case by updating the condition to not delete the featured image in case of draft of new page.

--- notes-service/src/main/java/org/exoplatform/wiki/service/impl/NoteServiceImpl.java
+++ notes-service/src/main/java/org/exoplatform/wiki/service/impl/NoteServiceImpl.java
@@ -1711,8 +1711,8 @@ import lombok.SneakyThrows;
     if (isDraft) {
       DraftPage draftPage = getDraftNoteById(String.valueOf(noteId),
                                              identityManager.getIdentity(String.valueOf(userIdentityId)).getRemoteId());
-      if (draftPage != null && draftPage.getTargetPageId() != null
-          && isOriginalFeaturedImage(draftPage, getNoteByIdAndLang(Long.valueOf(draftPage.getTargetPageId()), lang))) {
+      if (draftPage != null && (draftPage.getTargetPageId() == null
+          || isOriginalFeaturedImage(draftPage, getNoteByIdAndLang(Long.valueOf(draftPage.getTargetPageId()), lang)))) {
         removeFeaturedImageFile = false;
       }
       note = draftPage;
@@ -1997,6 +1997,20 @@ import lombok.SneakyThrows;
                   lang,
                   true,
                   Long.parseLong(identityManager.getOrCreateUserIdentity(note.getOwner()).getId()));
+        } else if (note.isDraftPage()) {
+          // When delete a draft of non-existing page
+          // check if its featured image was linked to a saved page
+          List<MetadataItem> metadataItems =
+                                           metadataService.getMetadataItemsByMetadataNameAndTypeAndObjectAndMetadataItemProperty(NOTES_METADATA_TYPE.getName(),
+                                                                                                                                 NOTES_METADATA_TYPE.getName(),
+                                                                                                                                 NOTE_METADATA_PAGE_OBJECT_TYPE,
+                                                                                                                                 FEATURED_IMAGE_ID,
+                                                                                                                                 featuredImageId,
+                                                                                                                                 0,
+                                                                                                                                 0);
+          if (metadataItems == null || metadataItems.isEmpty()) {
+            fileService.deleteFile(Long.parseLong(featuredImageId));
+          }
         } else {
           fileService.deleteFile(Long.parseLong(featuredImageId));
         }
