914cb36360088c680e217717f2fecd544e70f696
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-12-19 11:00:43 +0100

    feat: Create terms and conditions - MEED-7930 - Meeds-io/MIPs#157 (#1259)
    This PR will add a new item in general setting main for create terms and
    condition.

---
+++ notes-webapp/src/main/webapp/vue-app/general-settings-extension/components/TermsAndConditionsDrawer.vue
@@ -0,0 +1,69 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <exo-drawer
+    ref="drawer"
+    v-model="drawer"
+    id="termsAndConditionsDrawer"
+    allow-expand
+    right>
+    <template #title>
+      {{ $t('generalSettings.termsAndConditions') }}
+    </template>
+    <template v-if="drawer" #content>
+      <v-card class="pa-4" flat>
+        <div>
+          {{ $t('generalSettings.termsAndConditions.description') }}
+        </div>
+        <div class="d-flex align-center justify-center mt-4">
+          <v-btn
+            class="btn btn-primary"
+            @click="addTerms">
+            {{ $t('generalSettings.termsAndConditions.create') }}
+          </v-btn>
+        </div>
+      </v-card>
+    </template>
+  </exo-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+  }),
+  created() {
+    this.$root.$on('terms-and-conditions-create', this.open);
+  },
+  methods: {
+    open() {
+      this.$refs.drawer.open();
+    },
+    close() {
+      this.$refs.drawer.close();
+    },
+    addTerms() {
+      window.open(`${eXo.env.portal.context}/${eXo.env.portal.portalName}/notes-editor?showMaxWindow=true&hideSharedLayout=true&owner=__system&type=terms`, '_blank');
+    },
+  },
+};
+</script>
+
