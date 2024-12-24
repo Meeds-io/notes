338f24262e382c6742035a6809e499c1900bfdfe
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-12-24 16:15:51 +0100

    feat: Save terms and conditions - MEED-7945 - Meeds-io/MIPs#157 (#1261)
    This PR will allow admins to create/update terms and conditions note
    page

---
+++ notes-webapp/src/main/webapp/vue-app/general-settings-extension/js/TermsAndConditionsService.js
@@ -0,0 +1,62 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+export function getTermsAndConditions(lang) {
+  const formData = new FormData();
+  if (lang) {
+    formData.append('lang', lang);
+  }
+  const urlParams = new URLSearchParams(formData).toString();
+  const params = urlParams.length && `?${decodeURIComponent(urlParams)}` || '';
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/notes/terms${params}`, {
+    method: 'GET',
+    credentials: 'include',
+  }).then((resp) => {
+    if (resp?.ok) {
+      return resp.json();
+    } else if (resp?.status === 404) {
+      return null;
+    } else {
+      throw new Error('Error getting note page setting');
+    }
+  });
+}
+
+export function saveTermsAndConditions(content, lang) {
+  const formData = new FormData();
+  if (lang) {
+    formData.append('lang', lang);
+  }
+  formData.append('content', content || '');
+  const params = new URLSearchParams(formData).toString();
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/notes/terms`, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    method: 'PUT',
+    credentials: 'include',
+    body: params,
+  }).then((resp) => {
+    if (resp?.ok) {
+      return resp.json();
+    } else {
+      throw new Error('Error saving note page setting');
+    }
+  });
+}
