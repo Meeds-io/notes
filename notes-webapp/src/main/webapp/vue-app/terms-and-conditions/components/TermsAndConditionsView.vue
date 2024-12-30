4f332c9fae83e334ea1b2b847f37e233c06b8d02
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-12-30 13:40:45 +0100

    feat: Information and Acceptation of T&C - MEED-7954 - Meeds-io/MIPs#157 (#1264)
    This PR will implement user view of terms and conditions in a new page

---
+++ notes-webapp/src/main/webapp/vue-app/terms-and-conditions/components/TermsAndConditionsView.vue
@@ -0,0 +1,104 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <v-app v-if="pageContent" class="application-body">
+    <v-card flat>
+      <v-sheet height="12" class="primary no-border-bottom" />
+      <v-card-title class="text-h4 font-weight-bold text-color">{{ pageTitle }}</v-card-title>
+      <v-card-text
+        v-sanitized-html="pageContentDisplay" />
+      <v-card-actions class="px-4">
+        <div :style="brandingLogoDisplay">
+          <v-img
+            :src="brandingLogoUrl"
+            width="9em"
+            max-width="150px"
+            max-height="6em"
+            role="presentation"
+            contain
+            eager />
+        </div>
+        <div class="d-flex flex-column ms-auto">
+          <v-checkbox
+            v-model="accepted"
+            class="mb-2"
+            dense>
+            <template #label>
+              <span class="text--color font-weight-bold">
+                {{ $t('termsAndConditions.label.readAndConsent') }}
+              </span>
+            </template>
+          </v-checkbox>
+          <v-btn
+            :disabled="!accepted"
+            class="width-fit-content align-self-end"
+            color="primary"
+            depressed
+            @click="accept">
+            {{ $t('termsAndConditions.label.accept') }}
+          </v-btn>
+        </div>
+      </v-card-actions>
+    </v-card>
+  </v-app>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      accepted: false,
+      page: null,
+      lang: eXo.env.portal.language,
+    };
+  },
+  computed: {
+    pageContent() {
+      return this.page?.content;
+    },
+    pageTitle() {
+      return this.page?.title;
+    },
+    pageContentDisplay() {
+      return this.pageContent && this.$noteUtils.getContentToDisplay(this.pageContent) || '';
+    },
+    brandingLogoDisplay() {
+      return this.$vuetify.breakpoint.width >= 1720 ? 'position: fixed; right: 3%;bottom: 3%' : '';
+    },
+    brandingLogoUrl() {
+      return `/portal/rest/v1/platform/branding/logo?v=${Date.now()}`;
+    },
+  },
+  created() {
+    this.retrieveTerms();
+  },
+  methods: {
+    retrieveTerms() {
+      return this.$termsAndConditionsService.getTermsAndConditions(this.lang).then(page => {
+        this.page = page;
+      });
+    },
+    accept() {
+      // TO DO
+    }
+  },
+};
+</script>
