f1dba1f1612dba7216c7bd90ee185dc31cf6ac01
Azmi Touil <atouil@exoplatform.com>
2025-01-13 11:11:29 +0100

    fix: Fix image is not displayed in T&C for not admin users - MEED-8050 - Meeds-io/MIPs#157
    Prior to this change, image is not displayed in T&C for not admin users

--- notes-service/src/main/java/org/exoplatform/wiki/service/impl/NoteServiceImpl.java
+++ notes-service/src/main/java/org/exoplatform/wiki/service/impl/NoteServiceImpl.java
@@ -66,7 +66,6 @@ import org.exoplatform.services.log.Log;
 import org.exoplatform.services.security.Identity;
 import org.exoplatform.services.security.IdentityConstants;
 import org.exoplatform.services.thumbnail.ImageThumbnailService;
-import org.exoplatform.social.common.service.HTMLUploadImageProcessor;
 import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
 import org.exoplatform.social.core.manager.IdentityManager;
 import org.exoplatform.social.core.space.model.Space;
@@ -112,6 +111,9 @@ import io.meeds.notes.service.NotePageViewService;
 import io.meeds.social.cms.service.CMSService;
 import lombok.Getter;
 import lombok.SneakyThrows;
+
+import static io.meeds.notes.service.TermsAndConditionsService.TC_NOTE_NAME;
+import static io.meeds.notes.service.TermsAndConditionsService.TC_NOTE_TYPE;
 
 
  public class NoteServiceImpl implements NoteService {
@@ -2107,9 +2109,14 @@ import lombok.SneakyThrows;
 
   private boolean canViewNotes(String authenticatedUser, Space space, Page page) throws WikiException {
     if (space != null) {
-      return !page.isDraftPage() ? spaceService.canViewSpace(space, authenticatedUser) : Utils.canManageNotes(authenticatedUser, space, page);
+      return !page.isDraftPage() ? spaceService.canViewSpace(space, authenticatedUser)
+                                 : Utils.canManageNotes(authenticatedUser, space, page);
+    } else if (TC_NOTE_TYPE.equals(page.getWikiType()) && TC_NOTE_NAME.equals(page.getName())) {
+      return true;
     } else if (StringUtils.equals(page.getOwner(), IdentityConstants.SYSTEM) || StringUtils.isBlank(page.getOwner())) {
-      return cmsService.hasAccessPermission(Utils.getIdentity(authenticatedUser), NotePageViewService.CMS_CONTENT_TYPE, page.getName());
+      return cmsService.hasAccessPermission(Utils.getIdentity(authenticatedUser),
+                                            NotePageViewService.CMS_CONTENT_TYPE,
+                                            page.getName());
     } else {
       return spaceService.isSuperManager(space, authenticatedUser) || StringUtils.equals(page.getOwner(), authenticatedUser);
     }
