64400a896aeed6df35add0cfd82db216ea86f8d7
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-05-09 15:30:19 +0100

    feat: Review notes search result display - MEED-8980 - Meeds-io/MIPs#193 (#1426)

--- notes-webapp/src/main/webapp/vue-app/wikiSearch/main.js
+++ notes-webapp/src/main/webapp/vue-app/wikiSearch/main.js
@@ -4,6 +4,7 @@ export function formatSearchResult(results,term) {
   if (results && results.jsonList && results.jsonList.length) {
     results = results.jsonList.map(note => {
       note.title = note.title.replace(new RegExp(`(${term})`, 'ig'), '<span class="searchMatchExcerpt">$1</span>');
+      note.summary = note.summary.replace(new RegExp(`(${term})`, 'ig'), '<span class="searchMatchExcerpt">$1</span>');
       note.excerpt = $('<div />').html(note.excerpt).text().replace(new RegExp(`(${term})`, 'ig'), '<span class="searchMatchExcerpt">$1</span>');
       return note;
     });
