215d63620ad9520e1035a5f01a3229f1bf54a394
Boubaker Khanfir <bkhanfir@exoplatform.com>
2025-05-30 07:53:14 +0100

    fix: Upgrade Hibernate to version 6.6 - Meeds-io/meeds#3365 (#1434)
    This change will upgrade Hibernate to version 6.6 which introduced a bug
    fix (as explained in
    https://discourse.hibernate.org/t/instance-save-transient-before/10293/2)
    which makes some operations buggy when made in the same transaction.
    This change ensures to split operations to not hit such a problem.

--- notes-service/src/main/java/org/exoplatform/wiki/jpa/dao/DraftPageDAO.java
+++ notes-service/src/main/java/org/exoplatform/wiki/jpa/dao/DraftPageDAO.java
@@ -1,22 +1,22 @@
- /**
- * This file is part of the Meeds project (https://meeds.io/).
- *
- * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
+/**
+* This file is part of the Meeds project (https://meeds.io/).
+*
+* Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program; if not, write to the Free Software Foundation,
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 package org.exoplatform.wiki.jpa.dao;
 
 import java.util.List;
@@ -28,18 +28,16 @@ import org.exoplatform.wiki.jpa.entity.DraftPageEntity;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.TypedQuery;
 
-/**
- * Created by The eXo Platform SAS
- * Author : eXoPlatform
- *          exo@exoplatform.com
- * Jun 24, 2015  
- */
 public class DraftPageDAO extends WikiBaseDAO<DraftPageEntity, Long> {
 
+  private static final String TARGET_PAGE_ID_PARAM = "targetPageId";
+
   public DraftPageEntity findDraftPageByName(String draftPageName) {
-    TypedQuery<DraftPageEntity> query = getEntityManager().createNamedQuery("wikiDraftPage.findDraftPageByName", DraftPageEntity.class)
-            .setMaxResults(1)
-            .setParameter("draftPageName", draftPageName);
+    TypedQuery<DraftPageEntity> query = getEntityManager()
+                                                          .createNamedQuery("wikiDraftPage.findDraftPageByName",
+                                                                            DraftPageEntity.class)
+                                                          .setMaxResults(1)
+                                                          .setParameter("draftPageName", draftPageName);
 
     try {
       return query.getSingleResult();
@@ -49,38 +47,48 @@ public class DraftPageDAO extends WikiBaseDAO<DraftPageEntity, Long> {
   }
 
   public List<DraftPageEntity> findDraftPagesByTargetPage(long targetPageId) {
-    TypedQuery<DraftPageEntity> query = getEntityManager().createNamedQuery("wikiDraftPage.findDraftPageByTargetPage", DraftPageEntity.class)
-            .setParameter("targetPageId", targetPageId);
-    return query.getResultList();
+    return getEntityManager().createNamedQuery("wikiDraftPage.findDraftPageByTargetPage", DraftPageEntity.class)
+                             .setParameter(TARGET_PAGE_ID_PARAM, targetPageId)
+                             .getResultList();
   }
 
   public List<DraftPageEntity> findDraftPagesByParentPage(long parentPageId) {
-    TypedQuery<DraftPageEntity> query = getEntityManager().createNamedQuery("wikiDraftPage.findDraftPagesByParentPage", DraftPageEntity.class)
-            .setParameter("parentPageId", parentPageId);
+    TypedQuery<DraftPageEntity> query = getEntityManager()
+                                                          .createNamedQuery("wikiDraftPage.findDraftPagesByParentPage",
+                                                                            DraftPageEntity.class)
+                                                          .setParameter("parentPageId", parentPageId);
     return query.getResultList();
   }
 
   @ExoTransactional
   public void deleteDraftPagesByTargetPage(long targetPageId) {
-
     List<DraftPageEntity> draftPages = findDraftPagesByTargetPage(targetPageId);
-    for (DraftPageEntity draftPage: draftPages) {
+    for (DraftPageEntity draftPage : draftPages) {
       delete(draftPage);
     }
+  }
 
+  @ExoTransactional
+  public void deleteDraftPagesByParentPage(long targetPageId) {
+    List<DraftPageEntity> draftPages = findDraftPagesByParentPage(targetPageId);
+    for (DraftPageEntity draftPage : draftPages) {
+      delete(draftPage);
+    }
   }
 
   @ExoTransactional
   public void deleteDraftPagesByName(String draftName) {
     DraftPageEntity draftPage = findDraftPageByName(draftName);
-    if(draftPage != null) {
+    if (draftPage != null) {
       delete(draftPage);
     }
   }
 
   public DraftPageEntity findLatestDraftPageByTargetPage(Long targetPageId) {
-    TypedQuery<DraftPageEntity> query = getEntityManager().createNamedQuery("wikiDraftPage.findLatestDraftPageByTargetPage", DraftPageEntity.class)
-            .setParameter("targetPageId", targetPageId);
+    TypedQuery<DraftPageEntity> query = getEntityManager()
+                                                          .createNamedQuery("wikiDraftPage.findLatestDraftPageByTargetPage",
+                                                                            DraftPageEntity.class)
+                                                          .setParameter(TARGET_PAGE_ID_PARAM, targetPageId);
 
     try {
       query.setMaxResults(1);
@@ -101,7 +109,7 @@ public class DraftPageDAO extends WikiBaseDAO<DraftPageEntity, Long> {
     TypedQuery<DraftPageEntity> query =
                                       getEntityManager().createNamedQuery("wikiDraftPage.findLatestDraftPageByTargetPageAndLang",
                                                                           DraftPageEntity.class)
-                                                        .setParameter("targetPageId", targetPageId)
+                                                        .setParameter(TARGET_PAGE_ID_PARAM, targetPageId)
                                                         .setParameter("lang", lang);
     query.setMaxResults(1);
     try {
