61e5b2ad35efb81c5598662309ecd1c9491c048b
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-07-10 16:33:10 +0100

    feat: Add Favorite Extension Label and Icon - MEED-9430 - Meeds-io/MIPs#209 This change will add Favorite Extension Label and Icon to display when Favorite Drawer is expanded.

--- notes-webapp/src/main/webapp/vue-app/notes-favorite-drawer-extension/extensions.js
+++ notes-webapp/src/main/webapp/vue-app/notes-favorite-drawer-extension/extensions.js
@@ -1,4 +1,9 @@
 export function initNotesExtensions() {
+  extensionRegistry.registerExtension('favorite', 'favorite-type', {
+    rank: 30,
+    id: 'notes',
+    icon: 'fa-clipboard',
+  });
   extensionRegistry.registerComponent('favorite-notes', 'favorite-drawer-item', {
     id: 'notes',
     vueComponent: Vue.options.components['notes-favorite-item'],
