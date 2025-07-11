7cee02b88fc44960a092d3b342d91d9c0ecfba87
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-07-11 13:02:05 +0100

    feat: Improve Favorite Note Display when drawer expanded - MEED-9435 - Meeds-io/MIPs#209

--- notes-webapp/src/main/webapp/vue-app/notes-favorite-drawer-extension/components/favorites/NotesFavoriteItem.vue
+++ notes-webapp/src/main/webapp/vue-app/notes-favorite-drawer-extension/components/favorites/NotesFavoriteItem.vue
@@ -5,14 +5,28 @@
     @auxclick="setAsViewed"
     @click="setAsViewed">
     <v-list-item-icon class="me-3 my-auto">
-      <v-img
-        :src="noteImg"
-        max-height="28"
-        max-width="25" />
+      <v-icon :size="iconSize">fa-clipboard</v-icon>
     </v-list-item-icon>
-
     <v-list-item-content>
-      <v-list-item-title class="text-color text-truncate">{{ noteTitle }}</v-list-item-title>
+      <v-list-item-title class="text-truncate">{{ noteTitle }}</v-list-item-title>
+      <v-list-item-subtitle v-if="expanded" class="d-flex align-center full-width overflow-hidden pt-2px">
+        <template v-if="spaceGroupId">
+          <space-avatar
+            :space-group-id="spaceGroupId"
+            :size="25"
+            class="flex-grow-0 flex-shrink-1 text-truncate"
+            link-style />
+          <v-icon class="flex-grow-0 flex-shrink-0 mx-2" size="2">fa-circle</v-icon>
+        </template>
+        <date-format class="flex-grow-0 flex-shrink-0" :value="updatedDate" />
+        <template v-if="author">
+          <v-icon class="flex-grow-0 flex-shrink-0 mx-2" size="2">fa-circle</v-icon>
+          <user-avatar
+            :profile-id="author"
+            :size="25"
+            class="flex-grow-1 flex-shrink-1 text-truncate" />
+        </template>
+      </v-list-item-subtitle>
     </v-list-item-content>
 
     <v-list-item-action>
@@ -47,15 +61,26 @@ export default {
   data: () => ({ 
     isFavorite: true,
     note: {},
-    noteImg: '/notes/images/notes-appicon.png',
   }),
   computed: {
     noteTitle() {
       return this.note?.title || '';
     },
+    updatedDate() {
+      return this.note?.updatedDate?.time || this.note?.createdDate?.time;
+    },
+    author() {
+      return this.note?.lastUpdater || this.note?.author;
+    },
     noteUrl() {
-      return this.note.lang && `${this.note?.url}?translation=${this.note.lang}` || `${this.note?.url}?translation=original`;
-    }
+      return this.note?.lang && `${this.note?.url}?translation=${this.note.lang}` || `${this.note?.url}?translation=original`;
+    },
+    spaceGroupId() {
+      return this.note?.wikiType === 'group' &&  this.note?.wikiOwner?.startsWith?.('/spaces/') ? this.note?.wikiOwner : null;
+    },
+    iconSize() {
+      return this.expanded ? 34 : 24;
+    },
   },
   created() {
     let noteId = this.id;
