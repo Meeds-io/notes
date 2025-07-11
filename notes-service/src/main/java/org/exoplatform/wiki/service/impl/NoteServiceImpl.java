0fb1f73be3a0bd3129c4fc6b72281cd000263a74
Anwar khanfir <93767376+akhanfir@users.noreply.github.com>
2025-07-11 09:40:40 +0100

    fix: can't update existing note by import in same space - meed-9396 - meeds-io/meeds#3469  (#1470)
    Before this change, when try to import a note and from Import rules choose update existing notes and duplicate existing notes then click on import, the note is not imported and error popup is displayed. To fix this problem, when creating note change pass importMode as value equal to true to create another name. After this change, the note is imported.
    Resolves https://github.com/Meeds-io/meeds/issues/3469

--- notes-service/src/main/java/org/exoplatform/wiki/service/impl/NoteServiceImpl.java
+++ notes-service/src/main/java/org/exoplatform/wiki/service/impl/NoteServiceImpl.java
@@ -1955,7 +1955,7 @@ public class NoteServiceImpl implements NoteService {
             targetNote = note_;
           }
           if (conflict.equals("duplicate")) {
-            note_ = createNote(wiki, parent_.getName(), note, userIdentity, false, true);
+            note_ = createNote(wiki, parent_.getName(), note, userIdentity, true, true);
             targetNote = note_;
           }
           if (conflict.equals("update")) {
