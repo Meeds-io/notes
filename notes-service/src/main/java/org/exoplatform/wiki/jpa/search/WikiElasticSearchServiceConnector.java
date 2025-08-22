4474cb7352e19d523a8a04e6cc7c3e6bf75fbd50
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-08-22 16:32:28 +0100

    fix: handle unsupported sort fields by returning default sorting query - MEED-9574 - Meeds-io/MIPs#212 (#1504)

--- notes-service/src/main/java/org/exoplatform/wiki/jpa/search/WikiElasticSearchServiceConnector.java
+++ notes-service/src/main/java/org/exoplatform/wiki/jpa/search/WikiElasticSearchServiceConnector.java
@@ -443,7 +443,7 @@ public class WikiElasticSearchServiceConnector extends ElasticSearchServiceConne
     }
     return switch (sortFiled) {
       case "date" -> SORTING_QUERY.replace("@sortField@", "lastUpdatedDate").replace("@sortOrder@", sortDirection);
-      default -> SORTING_QUERY.replace("@sortField@", sortFiled).replace("@sortOrder@", sortDirection);
+      default -> DEFAULT_SORTING_QUERY;
     };
   }
 
