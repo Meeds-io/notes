995c1cd0725c8b4978f8361690332651933b4db4
Boubaker Khanfir <bkhanfir@exoplatform.com>
2025-08-25 09:48:04 +0100

    feat: Update Java and XML License Header - Meeds-io/meeds#3324 (#1505) (#1506)

--- notes-service/src/main/java/org/exoplatform/wiki/jpa/dao/WikiDAO.java
+++ notes-service/src/main/java/org/exoplatform/wiki/jpa/dao/WikiDAO.java
@@ -1,7 +1,7 @@
-/*
+/**
  * This file is part of the Meeds project (https://meeds.io/).
  *
- * Copyright (C) 2020 - 2022 Meeds Association contact@meeds.io
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -9,14 +9,13 @@
  * version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-
 package org.exoplatform.wiki.jpa.dao;
 
 import java.util.List;
@@ -28,10 +27,6 @@ import org.exoplatform.commons.api.persistence.ExoTransactional;
 import org.exoplatform.wiki.jpa.entity.WikiEntity;
 import org.exoplatform.wiki.model.WikiType;
 
-/**
- * Created by The eXo Platform SAS Author : eXoPlatform exo@exoplatform.com Jun
- * 24, 2015
- */
 public class WikiDAO extends WikiBaseDAO<WikiEntity, Long> {
 
   private PageDAO pageDAO;
