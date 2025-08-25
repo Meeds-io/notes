673244769915590b2467a1fffa89defc176a2a4d
Boubaker Khanfir <bkhanfir@exoplatform.com>
2025-08-25 08:36:35 +0100

    feat: Update Java and XML License Header - Meeds-io/meeds#3324 (#1505)

--- notes-service/src/main/java/org/exoplatform/wiki/jpa/entity/DraftPageEntity.java
+++ notes-service/src/main/java/org/exoplatform/wiki/jpa/entity/DraftPageEntity.java
@@ -1,13 +1,12 @@
- /**
+/**
  * This file is part of the Meeds project (https://meeds.io/).
  *
- * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -30,12 +29,6 @@ import jakarta.persistence.NamedQuery;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
-/**
- * Created by The eXo Platform SAS
- * Author : eXoPlatform
- * exo@exoplatform.com
- * Jun 23, 2015
- */
 @Entity(name = "WikiDraftPageEntity")
 @Table(name = "WIKI_DRAFT_PAGES")
 @NamedQuery(name = "wikiDraftPage.findDraftPages", query = "SELECT d FROM WikiDraftPageEntity d ORDER BY d.updatedDate DESC")
--- notes-service/src/main/java/org/exoplatform/wiki/jpa/search/WikiElasticSearchServiceConnector.java
+++ notes-service/src/main/java/org/exoplatform/wiki/jpa/search/WikiElasticSearchServiceConnector.java
@@ -1,18 +1,20 @@
-/*
- * Copyright (C) 2003-2015 eXo Platform SAS.
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
  *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see http://www.gnu.org/licenses/ .
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package org.exoplatform.wiki.jpa.search;
 
@@ -54,10 +56,6 @@ import org.exoplatform.wiki.utils.Utils;
 
 import io.meeds.notes.legacy.search.es.ElasticSearchServiceConnector;
 
-/**
- * Created by The eXo Platform SAS Author : Thibault Clement
- * tclement@exoplatform.com 11/24/15
- */
 public class WikiElasticSearchServiceConnector extends ElasticSearchServiceConnector {
 
   private static final Log           LOG                                     =
