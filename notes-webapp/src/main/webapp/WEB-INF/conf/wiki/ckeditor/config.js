508e858e0bad9f842ef12916e0a0dde780104fb4
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-12-24 14:22:15 +0100

    feat: Add Insert Content Link Extension as last Buttin in Rich Editor - EXO-83433

--- notes-webapp/src/main/webapp/WEB-INF/conf/wiki/ckeditor/config.js
+++ notes-webapp/src/main/webapp/WEB-INF/conf/wiki/ckeditor/config.js
@@ -73,7 +73,7 @@ CKEDITOR.editorConfig = function (config) {
   if (!webPageNote) {
     mobileToolbar[mobileToolbar.findIndex(item => item.name ==='blocks')].items.push('attachFile');
   }
-  let extraPlugins = `a11ychecker,balloonpanel,indent,indentblock,indentlist,codesnippet,sharedspace,copyformatting,table,tabletools,embedsemantic,autolink,colordialog,emoji,link,font,justify,widget${!webPageNote && ',suggester,tagSuggester' || ''},contextmenu,tabletools,tableresize,toc,linkBalloon,image2,insertImage,insertContentLink,vuetifyStyle`;
+  let extraPlugins = `a11ychecker,balloonpanel,indent,indentblock,indentlist,codesnippet,sharedspace,copyformatting,table,tabletools,embedsemantic,autolink,colordialog,emoji,link,font,justify,widget${!webPageNote && ',suggester,tagSuggester' || ''},contextmenu,tabletools,tableresize,toc,linkBalloon,image2,insertImage,vuetifyStyle`;
   let removePlugins = `image,confirmBeforeReload,maximize,resize,autoembed${webPageNote && ',tagSuggester' || ''}`;
 
   let configOptions = {};
@@ -106,7 +106,7 @@ CKEDITOR.editorConfig = function (config) {
   } else {
     config.toolbar = toolbar;
   }
-  config.extraPlugins = extraPlugins;
+  config.extraPlugins = `${extraPlugins},insertContentLink`;
   config.removePlugins = removePlugins;
   config.toolbarGroups = [
     { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
