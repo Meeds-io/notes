e6cbd87412e48f938fd2e2ab7af8c7414d8a0330
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2026-01-19 10:07:26 +0100

    feat:Side navigation treeview in note application - MEED-10100 - Meeds-io/MIPs#235 (#1588)
    This change will implement a new side bar navigation for note
    application

--- notes-webapp/src/main/webapp/vue-app/notes-treeview/initComponents.js
+++ notes-webapp/src/main/webapp/vue-app/notes-treeview/initComponents.js
@@ -1,8 +1,10 @@
 import NoteBreadcrumb from '../notes/components/NoteBreadcrumb.vue';
 import NoteTreeviewDrawer from './components/NoteTreeviewDrawer.vue';
+import NoteTreeViewSideBar from './components/NoteTreeViewSideBar.vue';
 
 const components = {
   'note-treeview-drawer': NoteTreeviewDrawer,
+  'note-treeview-sideBar': NoteTreeViewSideBar,
   'note-breadcrumb': NoteBreadcrumb
 };
 
