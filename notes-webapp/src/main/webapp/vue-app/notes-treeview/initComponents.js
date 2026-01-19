1317c815bb5333fbf59a00ee06af2729bba4fe90
Azmi Touil <atouil@exoplatform.com>
2026-01-19 11:43:56 +0100

    feat:Side navigation treeview in note application - MEED-10100 - Meeds-io/MIPs#235
    Fix feedbacks

--- notes-webapp/src/main/webapp/vue-app/notes-treeview/initComponents.js
+++ notes-webapp/src/main/webapp/vue-app/notes-treeview/initComponents.js
@@ -1,10 +1,14 @@
 import NoteBreadcrumb from '../notes/components/NoteBreadcrumb.vue';
 import NoteTreeviewDrawer from './components/NoteTreeviewDrawer.vue';
 import NoteTreeViewSideBar from './components/NoteTreeViewSideBar.vue';
+import NoteTreeViewSideBarItemLabel from './components/NoteTreeViewSideBarItemLabel.vue';
+import NoteTreeViewSideBarItemPrepend from './components/NoteTreeViewSideBarItemPrepend.vue';
 
 const components = {
   'note-treeview-drawer': NoteTreeviewDrawer,
   'note-treeview-sideBar': NoteTreeViewSideBar,
+  'note-treeview-sidebar-item-label': NoteTreeViewSideBarItemLabel,
+  'note-treeview-sidebar-item-prepend': NoteTreeViewSideBarItemPrepend,
   'note-breadcrumb': NoteBreadcrumb
 };
 
