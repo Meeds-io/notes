e5f9ac06b4ac5e0f71b308410dd4ae9e493e6ca0
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2026-01-20 17:03:48 +0100

    feat: Filter by name and note type - MEED-10104 - Meeds-io/MIPs#235 (#1590)
    This change will implement a new toolbar for filter note

--- notes-webapp/src/main/webapp/vue-app/notes-treeview/initComponents.js
+++ notes-webapp/src/main/webapp/vue-app/notes-treeview/initComponents.js
@@ -3,12 +3,16 @@ import NoteTreeviewDrawer from './components/NoteTreeviewDrawer.vue';
 import NoteTreeViewSideBar from './components/NoteTreeViewSideBar.vue';
 import NoteTreeViewSideBarItemLabel from './components/NoteTreeViewSideBarItemLabel.vue';
 import NoteTreeViewSideBarItemPrepend from './components/NoteTreeViewSideBarItemPrepend.vue';
+import NoteTreeviewToolbar from './components/NoteTreeviewToolbar.vue';
+import NoteTreeViewFilterDrawer from './components/NoteTreeViewFilterDrawer.vue';
 
 const components = {
   'note-treeview-drawer': NoteTreeviewDrawer,
   'note-treeview-sideBar': NoteTreeViewSideBar,
   'note-treeview-sidebar-item-label': NoteTreeViewSideBarItemLabel,
   'note-treeview-sidebar-item-prepend': NoteTreeViewSideBarItemPrepend,
+  'note-treeview-toolbar': NoteTreeviewToolbar,
+  'note-treeview-filter-drawer': NoteTreeViewFilterDrawer,
   'note-breadcrumb': NoteBreadcrumb
 };
 
