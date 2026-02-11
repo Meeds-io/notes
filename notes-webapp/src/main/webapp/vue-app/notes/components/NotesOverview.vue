aa0b315058267036ef718b6c4389d0199d888b5b
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2026-02-11 11:19:20 +0100

    fix: Fix note display when increasing the treeview size - MEED-10235 - Meeds-io/MIPs#235 (#1616)
    This change will fix the display of note page when increasing the
    treeview size

--- notes-webapp/src/main/webapp/vue-app/notes/components/NotesOverview.vue
+++ notes-webapp/src/main/webapp/vue-app/notes/components/NotesOverview.vue
@@ -20,7 +20,7 @@
 -->
 <template>
   <v-container fluid class="pa-0">
-    <v-row no-gutters class="pa-5">
+    <v-row no-gutters class="pa-5 flex-nowrap">
       <v-sheet
         v-if="treeViewExpended && !$root.isMobile"
         :width="sidebarWidth"
