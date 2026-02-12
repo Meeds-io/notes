5b524ec3ead3a21cebfe3eb1f81882e7b99d30b7
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2026-02-12 14:12:19 +0100

    fix: Update UX when clicking on the avatar in "My Notes" to open the note instead of the user profile - MEED-10181 - Meeds-io/MIPs#235 (#1621)
    This change will update UX when clicking on the avatar in "My Notes" to
    open the note instead of the user profile

--- notes-webapp/src/main/webapp/vue-app/notes-treeview/components/NoteTreeViewSideBarItemPrepend.vue
+++ notes-webapp/src/main/webapp/vue-app/notes-treeview/components/NoteTreeViewSideBarItemPrepend.vue
@@ -19,27 +19,15 @@
 
 -->
 <template>
-  <div class="d-flex flex-grow-1">
-    <v-btn
-      v-if="hasChild"
-      :loading="isLoading"
-      icon
-      @click="$emit('fetch-children', note)">
-      <v-icon
-        size="24">
-        {{ open ? 'mdi-menu-down' : 'mdi-menu-right' }}
-      </v-icon>
-    </v-btn>
-    <template v-if="isHomePage">
-      <exo-user-avatar
-        v-if="!spaceNote"
-        :profile-id="noteWikiOwner"
-        :size="24"
-        :popover="false"
-        extra-class="align-self-center"
-        avatar />
-    </template>
-  </div>
+  <v-btn
+    v-if="hasChild"
+    :loading="isLoading"
+    icon
+    @click="$emit('fetch-children', note)">
+    <v-icon size="20">
+      {{ open ? 'mdi-menu-down' : 'mdi-menu-right' }}
+    </v-icon>
+  </v-btn>
 </template>
 
 <script>
@@ -49,26 +37,10 @@ export default {
       type: Object,
       default: null
     },
-    noteWikiOwner: {
-      type: String,
-      default: null,
-    },
-    spaceGroupId: {
-      type: String,
-      default: null,
-    },
-    spaceNote: {
-      type: Boolean,
-      default: false
-    },
     open: {
       type: Boolean,
       default: false
     },
-    isDraftFilter: {
-      type: Boolean,
-      default: false
-    }
   },
   data() {
     return {
@@ -76,18 +48,12 @@ export default {
     };
   },
   computed: {
-    noteId() {
-      return this.note.noteId;
-    },
     hasChild() {
       return this.note.hasChild;
     },
     isLoading() {
       return this.note.isLoading;
     },
-    isHomePage() {
-      return this.note.nodeType === 'WIKIHOME';
-    }
   }
 };
 </script>
